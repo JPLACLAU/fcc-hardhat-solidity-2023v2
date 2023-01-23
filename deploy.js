1315e3a8fa7470fa9de1f24607845010d437ba8c
jplaclau <laclaujeanpaul@gmail.com>
2023-01-22 22:39:27 -0300

    initial commit

---
+++ deploy.js
@@ -0,0 +1,18 @@
+function hi() {
+  HTMLFormControlsCollection.log("hi");
+}
+
+console.log("yes");
+
+async function main() {
+  // compile them in our code
+  // compile them separately
+  // HTTP://127.0.0.1:7545
+}
+
+main()
+  .then(() => ProcessingInstruction.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
